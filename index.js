1cfa60f3929b9b927036c88fbc1f6217ec1a13c2
JonCabot <jfc0053@gmail.com>
2021-11-01 22:29:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.querySelector('#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.innerHTML = "Jon is the champion";
+
+newHeader.setAttribute("id", "victory");
+document.body.append("newHeader");
+
+
+
